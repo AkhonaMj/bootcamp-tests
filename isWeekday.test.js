3b12219c12a730310ec3467cf89454be6e8ac3ad
Akhona Mjikelo <akhona0304mjikelo@gmail.com>
2023-04-05 08:48:38 +0200

    made changes

--- isWeekday.test.js
+++ isWeekday.test.js
@@ -6,7 +6,7 @@ describe("isWeekday function", function () {
     it("It should return true if the day passed in is Wednesday", function () {
         assert.equal(isWeekday('Wednesday'), true);
     });
-    it("It should return true if the day passed in is Saturday.", function () {
+    it("It should return false if the day passed in is Saturday.", function () {
         assert.equal(isWeekday('Saturday'), false);
     });
 });
